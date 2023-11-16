07e5a06584ac55f68693838d2ef524c38678600e
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-16 17:25:50 -0300

    Add RemoteCo module

--- app/scrapers/remote_co.rb
+++ app/scrapers/remote_co.rb
@@ -1,23 +1,9 @@
-class RemoteCo
+class RemoteCo < Driver
 
-  def initialize
-    require "selenium-webdriver"
-
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--disable-translate')
-
-    @driver = Selenium::WebDriver.for(
-      :remote,
-      url: "http://#{ENV['SELENIUM_HOST']}:4444/wd/hub",
-      options: options
-      )
-  end
+  SEARCH_KEY = "https://remote.co/remote-jobs/search/?search_keywords=ruby"
 
   def search
-    @driver.navigate.to "https://remote.co/remote-jobs/search/?search_keywords=ruby"
-
+    @driver.navigate.to SEARCH_KEY
     wait = Selenium::WebDriver::Wait.new
     begin
       wait.until { @driver.find_elements(:class, "job_listings")[-1].text != "Loading..." }
@@ -27,17 +13,17 @@ class RemoteCo
         company = element.find_elements(:class, 'm-0')[1].text.split('|')[0].strip
         link = element.find_element(:css, 'a').attribute("href")
         website = "remote.co"
-        if ::Model::Job.find_by(link: link).nil?
-          ::Model::Job.create!(title:, company:, link:, website:)
+        if Position.find_by(link: link).nil?
+          Position.create!(title:, company:, link:, website:)
         end
       end
     rescue => e
-      p "#{e.message}"
-    ensure
-      @driver.quit
+      p e.message
     end
   end
-end
 
-remote_co = RemoteCo.new
-remote_co.search
+  def quit
+    p 'Quitting driver'
+    @driver.quit
+  end
+end
