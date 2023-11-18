f4ac32154dbe16e603b48cd42a04e553258386da
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-18 15:50:00 -0300

    Reverse order of display

--- app/controllers/position_controller.rb
+++ app/controllers/position_controller.rb
@@ -1,6 +1,6 @@
 class PositionController < ApplicationController
   def index
-    @positions = Position.where(created_at: Time.current.all_day)
+    @positions = Position.where(created_at: Time.current.all_day).order('created_at ASC').reverse_order
   end
 
   def scraper
