59085f9f8507e7b5f4e28ce84c4795cd7181cc0e
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-19 11:48:30 -0300

    Add Scraper class

---
+++ app/scrapers/scraper.rb
@@ -0,0 +1,37 @@
+require "./config/environment.rb"
+
+class Scraper
+
+  def self.scrape
+    remote_co
+    ruby_on_remote_junior
+    ruby_on_remote_mid_level
+    working_nomads
+  end
+
+  private
+
+  def self.remote_co
+    remote_co = RemoteCo.new
+    remote_co.search
+    remote_co.quit
+  end
+
+  def self.ruby_on_remote_junior
+    ruby_on_remote_junior = RubyOnRemoteJunior.new
+    ruby_on_remote_junior.search
+    ruby_on_remote_junior.quit
+  end
+
+  def self.ruby_on_remote_mid_level
+    ruby_on_remote_junior = RubyOnRemoteMidLevel.new
+    ruby_on_remote_junior.search
+    ruby_on_remote_junior.quit
+  end
+
+  def self.working_nomads
+    workingnomads = WorkingNomands.new
+    workingnomads.search
+    workingnomads.quit
+  end
+end
